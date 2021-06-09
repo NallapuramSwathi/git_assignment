58a389650f0c9007bac36037f18a5f9b53a32bb3
NallapuramSwathi <swathinallapuram@gmail.com>
2021-06-09 11:29:52 +0530

    commited

---
+++ calculator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class calculator{
+	public static void main(String[] args){
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int add = a+b;
+		int diff = a-b;
+		System.out.println(add);
+		System.out.println(diff);
+	}
+}
+
+	
