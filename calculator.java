41e840cd2375ab584ad8d143244ea18791df6077
NallapuramSwathi <swathinallapuram@gmail.com>
2021-06-09 13:37:10 +0530

    4x_commited

---
+++ calculator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class calculator{
+	public static void main(String[] args){
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int add = 4*a+4*b;
+		int diff = 4*a-4*b;
+		System.out.println(add);
+		System.out.println(diff);
+	}
+}
+
+
